f6dd6db3e37ba21ab9eec4f5c962d47072b55c5a
Alex <alexferraro35@gmail.com>
2025-07-26 17:38:13 -0400

    bug fix: pinned prebuilt blink to quiet a warning

--- lua/plugins/lazydev.lua
+++ lua/plugins/lazydev.lua
@@ -1,3 +1,5 @@
+-- lua/plugins/lazydev.lua
+
 return {
   {
     "folke/lazydev.nvim",
@@ -22,6 +24,7 @@ return {
   },
   { -- optional blink completion source for require statements and module annotations
     "saghen/blink.cmp",
+    version = "1.*",
     opts = {
       sources = {
         -- add lazydev to your completion providers
