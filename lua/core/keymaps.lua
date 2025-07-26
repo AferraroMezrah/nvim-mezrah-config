6a82e3d77de4b3b12a6cd8d659ef0ea214345ee4
Alex <alexferraro35@gmail.com>
2025-07-26 15:07:00 -0400

    feat(core): primeagen inspired options

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -13,3 +13,32 @@ vim.keymap.set("n", "<leader>fp", function()
   require("core.colorscheme_picker").pick()
 end, { desc = "Pick colorscheme with preview" })
 
+-- Primeagen inspired
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set("n", "J", "mzJ`z")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- "greatest remap ever
+vim.keymap.set("x", "<leader>p", "\"_dp")
+
+-- "next greateset remap ever : asbjornHaland"
+vim.keymap.set("n", "<leader>y", "\"+y")
+vim.keymap.set("v", "<leader>y", "\"+y")
+vim.keymap.set("n", "<leader>Y", "\"+y")
+
+vim.keymap.set("n", "<leader>d", "\"_d")
+vim.keymap.set("v", "<leader>d", "\"_d")
+
+vim.keymap.set("n", "Q", "<nop>")
+
+vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
+
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
--- lua/core/options.lua
+++ lua/core/options.lua
@@ -1,14 +1,39 @@
 -- lua/core/options.lua
 
+vim.opt.guicursor = ""
+
 vim.opt.number = true
 vim.opt.relativenumber = true
-vim.opt.wrap = false
+
 vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+
+vim.opt.wrap = false
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+local undodir = vim.fn.stdpath("state") .. "/undodir"
+vim.fn.mkdir(undodir, "p")  -- create dir if it doesn't exist
+vim.opt.undodir = undodir
+vim.opt.undofile = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
 vim.opt.cursorline = true
+
 vim.opt.termguicolors = true
+
 vim.opt.scrolloff = 8
 vim.opt.signcolumn = "yes"
+vim.opt.isfname:append("@-@")
+
+vim.opt.updatetime = 50
+
+vim.opt.colorcolumn = "80"
 
 vim.g.have_nerd_font = true
